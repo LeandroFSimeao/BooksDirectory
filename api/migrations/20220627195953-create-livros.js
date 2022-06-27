828e0928dfa5310e7c77a64af1774cea01791531
Leandro Simeão <leandrosimeao@yahoo.com.br>
2022-06-27 17:12:30 -0300

    Criando migração para o banco de dados

---
+++ api/migrations/20220627195953-create-livros.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Livros', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      título: {
+        type: Sequelize.STRING
+      },
+      autor: {
+        type: Sequelize.STRING
+      },
+      editora: {
+        type: Sequelize.STRING
+      },
+      numeroPaginas: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Livros');
+  }
+};
